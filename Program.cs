d4c7f38983bf1c1aca73fab79551d5345c08f5bd
goelpal <pallavisingha@gmail.com>
2024-01-03 11:08:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int[] myInts = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
+//Without LINQ
+/*foreach(var item in myInts)
+{
+    if(item>3)
+        Console.WriteLine(item);
+}*/
+
+//With LINQ
+
+var SelectedItems = myInts.Where(items => items > 4);
+
+foreach(var item in SelectedItems)
+{ Console.WriteLine(item); }
+
+int newSum = myInts.Sum();
+Console.WriteLine("The sum of numbers is :" +newSum);
+
+int SelectedSum = SelectedItems.Sum();
+Console.WriteLine("The sum of seelcted numbers is :" + SelectedSum);
+
